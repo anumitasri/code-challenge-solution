8a6b4683ccb72e51cef55eabfce74f5c366c49ea
anumita srivastava <anumitasrivastava@anumitas-MBP-2.attlocal.net>
2020-05-03 16:42:57 -0400

    done with final testing

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This is a test app to demonstrate CI/CD tools, automated testing and containerization.
         </p>
       </div>
     );
